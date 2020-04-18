7d17d4f9e7a7d0cea57918b9bb4ed2fb776ca742
Vany Liu <vany8462@gmail.com>
2020-04-18 10:25:26 +0800

    Add guess count

--- number_guessing.py
+++ number_guessing.py
@@ -1,14 +1,17 @@
 import random
 
 r = random.randint(1,100)
+count = 0
 
 while True:
 	user_input = input('Please enter number: ')
-	user_input = int(user_input)
+	count += 1
+	user_input = int(user_input) #casting
 	if user_input == r:
-		print('Correct!')
+		print('Correct!', count, 'times guessing')
 		break
 	elif user_input < r:
 		print('Higher!')
 	elif user_input > r:
 		print('Lower!')
+	print('Guess count: ', count)
